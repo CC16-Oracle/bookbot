d3e5e6c37231519a9b0157c3bf5587cc3e8609b9
CC16-Oracle <lcstompkins@gmail.com>
2025-03-23 15:54:12 -0400

    Added a word counter

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("greetings boots")
+def main():
+    def get_book_text(path):
+        with open(path) as file:
+            file_contents = file.read()
+        return file_contents
+
+    def num_words(file_contents):
+        return len(file_contents.split())
+    
+    word_count = num_words(get_book_text("books/frankenstein.txt"))
+        
+    
+    print(f"{word_count} words found in the document.")
+
+
+
+
+
+main()
