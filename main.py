365e504936809319e84307bb697d404f8e67a843
CC16-Oracle <lcstompkins@gmail.com>
2025-04-03 04:28:23 -0400

    Removed hard-coded paths, added mobydick.txt and prideandprejudice.txt

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import sys
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    pass
+    sys.exit(1)
 def main():
     def get_book_text(path):
         with open(path) as file:
@@ -6,15 +11,15 @@ def main():
 
 
     from stats import get_num_words, get_letter_frequency
-    char_freq = get_letter_frequency(get_book_text("books/frankenstein.txt"))
+    char_freq = get_letter_frequency(get_book_text(sys.argv[1]))
     
         
     
-    print(f"{get_num_words(get_book_text("books/frankenstein.txt"))} words found in the document.")
+    print(f"{get_num_words(get_book_text(sys.argv[1]))} words found in the document.")
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
-    print(f"Found {get_num_words(get_book_text("books/frankenstein.txt"))} total words")
+    print(f"Found {get_num_words(get_book_text(sys.argv[1]))} total words")
     print("--------- Character Count -------")
     for i in range(0, len(char_freq)):
         print(f"{char_freq[i][0]}: {char_freq[i][1]}")
